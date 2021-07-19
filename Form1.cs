dc490e5b7c396657a40c9fbb16eb022083cb716a
Joanna <aska3001@wp.pl>
2021-07-19 23:16:15 +0200

    done

--- Form1.cs
+++ Form1.cs
@@ -20,12 +20,31 @@ namespace Swap
             InitializeComponent();
 
             lucinda = new Elephant() { Name = "Lucinda", Earsize = 33 };
-            lloyd = new Elephant() { Name = "Lloyd", Earsize = 40 );
+            lloyd = new Elephant() { Name = "Lloyd", Earsize = 40 };
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant bufor = lloyd;
+            lloyd = lucinda;
+            lucinda = bufor;
+
+            MessageBox.Show("Objects swapped");
         }
     }
 }
